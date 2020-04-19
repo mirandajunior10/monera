7182d8810de17e7330e190fdcbcb7e46f78d9f40
Urquiza <urquiza-nobrega@outlook.com>
2020-04-19 12:25:07 -0300

    Ajustes

--- App/screens/Home/HomeScreen.js
+++ App/screens/Home/HomeScreen.js
@@ -1,7 +1,8 @@
 import styles from './styles';
 import React, { Component } from 'react';
 import { Icon } from "react-native-elements";
-import { Text, View } from 'react-native';
+import { Text, View } from 'react-native';  
+import PureChart from 'react-native-pure-chart';
 class HomeScreen extends Component {
   
     static navigationOptions = ({ navigation }) => ({
@@ -28,9 +29,33 @@ class HomeScreen extends Component {
       });
 
     render() {
+      let sampleData = [
+        {
+          seriesName: 'series1',
+          data: [
+            {x: '2018-02-01', y: 30},
+            {x: '2018-02-02', y: 200},
+            {x: '2018-02-03', y: 170},
+            {x: '2018-02-04', y: 250},
+            {x: '2018-02-05', y: 10}
+          ],
+          color: '#297AB1'
+        },
+        {
+          seriesName: 'series2',
+          data: [
+            {x: '2018-02-01', y: 20},
+            {x: '2018-02-02', y: 100},
+            {x: '2018-02-03', y: 140},
+            {x: '2018-02-04', y: 550},
+            {x: '2018-02-05', y: 40}
+          ],
+          color: 'yellow'
+        }
+      ]
         return (
             <View style={styles.container}>
-                <Text>Esta é a tela principal.</Text>
+             <PureChart data={sampleData} type='bar' />
             </View>
         );
     }
