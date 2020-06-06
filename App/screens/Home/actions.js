93b2c592385a6ae6853bfd96fd1089e1de62d071
Jorlan <jorlanjr@gmail.com>
2020-06-06 17:14:50 -0300

    Inserção botão novo no FAB

--- App/screens/Home/actions.js
+++ App/screens/Home/actions.js
@@ -10,6 +10,15 @@ var actions = actions = [{
   name: 'bt_nova_ordem',
   color: '#FBE158',
   position: 1,
+}, {
+  text: 'Botão novo aqui',
+  icon: <Icon
+    name="md-add"
+    type="ionicon"
+    color="white" />,
+  name: 'bt_novo',
+  color: '#FBE158',
+  position: 1,
 }]
 
 
